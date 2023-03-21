1363b10bec02c8d1ba0bef4641f8bf0cc8a46405
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-03-21 18:35:53 -0400

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -49,7 +49,7 @@ class DiscordClient
 			details: "In the Menus",
 			state: null,
 			largeImageKey: 'icon',
-			largeImageText: "Psych Engine"
+			largeImageText: "Panda Engine"
 		});
 	}
 
@@ -86,7 +86,7 @@ class DiscordClient
 			details: details,
 			state: state,
 			largeImageKey: 'icon',
-			largeImageText: "Engine Version: " + MainMenuState.psychEngineVersion,
+			largeImageText: "Engine Version: " + MainMenuState.PandaEngineVersion,
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
