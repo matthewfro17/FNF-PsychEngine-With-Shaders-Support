80dd5506f99b3f35e10c0e22ebd56c1eb6048577
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-03-21 18:01:28 -0400

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -174,7 +174,7 @@ class Paths
 
 	inline static public function json(key:String, ?library:String)
 	{
-		return getPath('data/$key.json', TEXT, library);
+		return getPath('data/charts/$key.json', TEXT, library);
 	}
 
 	inline static public function shaderFragment(key:String, ?library:String)
@@ -398,7 +398,7 @@ class Paths
 	}
 
 	inline static public function modsJson(key:String) {
-		return modFolders('data/' + key + '.json');
+		return modFolders('data/charts/' + key + '.json');
 	}
 
 	inline static public function modsVideo(key:String) {
